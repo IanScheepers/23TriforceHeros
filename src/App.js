e4b7f9f7f9f5ba14903bbbb65f8c6da7fda392e8
IanScheepers <104455565@student.swin.edu.au>
2024-10-30 00:08:47 +1100

    adding the how to run

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. the triforceheros soundtrack goes hard
+          Edit <code>src/App.js</code> and save to reload. the triforceheros soundtrack goess
         </p>
         <a
           className="App-link"
