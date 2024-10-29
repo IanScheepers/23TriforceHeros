ec3a0b267c2314ec90e65e41adff65bb384dd51d
IanScheepers <104455565@student.swin.edu.au>
2024-10-29 22:22:42 +1100

    testing something

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/triforceheros/i);
+  const linkElement = screen.getByText(/triforceheros2/i);
   expect(linkElement).toBeInTheDocument();
 });
