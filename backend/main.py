5d2dbdc2f470de4ef7eb161b91e9c39b635876ad
JaimeShirazi <73824309+JaimeShirazi@users.noreply.github.com>
2024-11-01 17:59:08 +1100

    oops

--- backend/main.py
+++ backend/main.py
@@ -59,31 +59,3 @@ async def predict_price(year: int, flights_flown: int, month: int, km: int):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
